d147442c6248672d4391c3cd9baf4bcc8a7a9f07
Franco Ezequiel Romero <93632921+francoeromero@users.noreply.github.com>
2024-05-07 17:25:06 -0300

    archviz

--- js/magazine.js
+++ js/magazine.js
@@ -408,4 +408,36 @@ flecha.click();
 
 setTimeout(function() {
     flecha.click();
-}, 5000);
+}, 8000);
+
+
+
+
+
+
+
+gsap.fromTo(
+	".loading-page",
+	{ opacity: 1 },
+	{
+	  opacity: 0,
+	  display: "none",
+	  duration: 1.5,
+	  delay: 3.5,
+	}
+  );
+  
+  gsap.fromTo(
+	".logo-name",
+	{
+	  y: 50,
+	  opacity: 0,
+	},
+	{
+	  y: 0,
+	  opacity: 1,
+	  duration: 2,
+	  delay: 0.5,
+	}
+  );
+  
